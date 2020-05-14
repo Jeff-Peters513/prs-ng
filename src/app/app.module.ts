fa746d01a564b704d4ded4b4a448fc7ba1e9aa81
Jeff Peters <petersjeff288@gmail.com>
2020-05-14 15:28:09 -0400

    added user components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './core/menu/menu.component';
+import { UserListComponent } from './feature/user/user-list/user-list.component';
+import { UserDetailComponent } from './feature/user/user-detail/user-detail.component';
+import { UserCreateComponent } from './feature/user/user-create/user-create.component';
+import { UserEditComponent } from './feature/user/user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserCreateComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule,
