ccbb616f294e7775c6173f7f4d005507a72decde
Jeff Peters <petersjeff288@gmail.com>
2020-05-15 16:14:56 -0400

    vendor components finished

--- src/app/feature/vendor/vendor-create/vendor-create.component.ts
+++ src/app/feature/vendor/vendor-create/vendor-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Vendor } from 'src/app/model/vendor.class';
+import { VendorService } from 'src/app/service/vendor.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-vendor-create',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendor-create.component.css']
 })
 export class VendorCreateComponent implements OnInit {
+  title: string = "Vendor-Create";
+  submitBtnTitle: string = "Create"
 
-  constructor() { }
+  vendor: Vendor = new Vendor();
+
+
+  constructor(private vendorSvc: VendorService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    //do nothing
   }
 
+  save() {
+    this.vendorSvc.create(this.vendor).subscribe(jr => {
+      //if jr.errors is null save was successful
+      if (jr.errors == null) {
+        //success
+        this.router.navigateByUrl("/vendor/list");
+      }
+      else {
+        console.log("*******Error creating new vendor: ", this.vendor, jr.errors);
+      }
+    });
+  }
 }
--- src/app/feature/vendor/vendor-edit/vendor-edit.component.ts
+++ src/app/feature/vendor/vendor-edit/vendor-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Vendor } from 'src/app/model/vendor.class';
+import { VendorService } from 'src/app/service/vendor.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-vendor-edit',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendor-edit.component.css']
 })
 export class VendorEditComponent implements OnInit {
+  title: string = "Vendor-Edit";
+  submitBtnTitle: string = "Edit";
+  vendor: Vendor = new Vendor();
+  vendorId: number = 0;
 
-  constructor() { }
+  constructor(private vendorSvc: VendorService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(parms => this.vendorId = parms["id"]);
+    this.vendorSvc.get(this.vendorId).subscribe(jr => {
+      this.vendor = jr.data as Vendor;
+    });
   }
 
+  save() {
+    this.vendorSvc.edit(this.vendor).subscribe(jr => {
+      if (jr.errors == null) {
+        //success
+        this.router.navigateByUrl("/vendor/list");
+      }
+      else {
+        console.log("***Error editing vendor.", this.vendor, jr.errors);
+      }
+    });
+  }
 }
